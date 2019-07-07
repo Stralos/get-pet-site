54fbb85a22ae29de485c2bb6c1ef55a7f3764823
Paulius Kiselis <paulius.kiselis@wahanda.com>
2019-07-07 20:06:04 +0300

    Added stats section.

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import style from './footer.module.scss';
+
+const Section = ({ head, content }) => (
+  <div className={style.section}>
+    <h4 className={style.sectionHead}>
+      {head}
+    </h4>
+    <div className={style.content}>
+      {content}
+    </div>
+  </div>
+);
+Section.propTypes = {
+  head: Proptypes.string.isRequired,
+  content: Proptypes.oneOfType([
+    Proptypes.string,
+    Proptypes.element,
+  ]).isRequired,
+};
+
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 export default class Footer extends React.PureComponent {
   render() {
     return (
-      <div className={style.footer}>
-        I am a footer
-      </div>
+      <footer className={style.footer}>
+        <Section head="APIE GETPET" content={text} />
+        <Section head="KONTAKTAI" content="labas@getpet.lt" />
+        <Section head="SOCIALINIAI TINKLAI" content="GetPet rasite:" />
+      </footer>
     );
   }
 }
--- src/components/Stats/stats.jsx
+++ src/components/Stats/stats.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import style from './stats.module.scss';
+
+const Stat = ({ stat, text }) => (
+  <li className={style.stat}>
+    <div className={style.number}>
+      {stat}
+    </div>
+    <div className={style.text}>
+      {text}
+    </div>
+  </li>
+);
+Stat.propTypes = {
+  stat: Proptypes.number.isRequired,
+  text: Proptypes.string.isRequired,
+};
 
 export default class Stats extends React.PureComponent {
   render() {
     return (
-      <div id="apie" className={style.stats}>
-        I am stats
-      </div>
+      <ul id="apie" className={style.stats}>
+        <Stat stat={1000} text="Parsisiuntimu" />
+        <Stat stat={2019} text="Parsisiuntimu" />
+        <Stat stat={777} text="Parsisiuntimu" />
+        <Stat stat={999} text="Parsisiuntimu" />
+      </ul>
     );
   }
 }
