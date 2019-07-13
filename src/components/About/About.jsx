375bf3ed5eea6df19cd832949797ce20ec7994a5
Paulius Kiselis <paulius.kiselis@wahanda.com>
2019-07-13 18:22:39 +0300

    Added icons as components.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import MiniText from 'src/components/common/atoms/MiniText';
 import leseLogo from 'src/assets/lese-logo.png';
-import Phone from 'src/assets/Phone.png';
-import PuppyQuestion from 'src/assets/Puppy-question.png';
-import PuppyWiggle from 'src/assets/Puppy-wiggle.png';
+import PuppyQuestion from 'src/assets/icons/PuppyQuestion';
+import PuppyWiggle from 'src/assets/icons/PuppyWiggle';
+import Phone from 'src/assets/icons/Phone';
 import sosGyvunaiLogo from 'src/assets/sos-gyvunai-logo.png';
 import vggnLogo from 'src/assets/vggn-logo.png';
 import GetToKnow from './GetToKnow';
@@ -16,44 +16,42 @@ export default class About extends React.PureComponent {
   render() {
     return (
       <div>
-        <div>
-          <ul className={style.instructionList}>
-            <li className={style.instruction}>
-              <GetToKnow
-                icon={<img src={Phone} alt="Phone" />}
-                header="Surask"
-                text={Loren}
-              />
-            </li>
-            <li className={style.instruction}>
-              <GetToKnow
-                icon={<img src={PuppyQuestion} alt="PuppyQuestion" />}
-                header="Pažink"
-                text={Loren}
-              />
-            </li>
-            <li className={style.instruction}>
-              <GetToKnow
-                icon={<img src={PuppyWiggle} alt="PuppyWiggle" />}
-                header="Augink"
-                text={Loren}
-              />
-            </li>
-          </ul>
+        <ul className={style.instructionList}>
+          <li className={style.instruction}>
+            <GetToKnow
+              icon={<Phone className={style.phone} />}
+              header="Surask"
+              text={Loren}
+            />
+          </li>
+          <li className={style.instruction}>
+            <GetToKnow
+              icon={<PuppyQuestion className={style.puppyQuestion} />}
+              header="Pažink"
+              text={Loren}
+            />
+          </li>
+          <li className={style.instruction}>
+            <GetToKnow
+              icon={<PuppyWiggle className={style.puppyWiggle} />}
+              header="Augink"
+              text={Loren}
+            />
+          </li>
+        </ul>
 
-          <MiniText text="Gyvūnų prieglaudos:" />
-          <ul className={style.shelters}>
-            <li className={style.shelter}>
-              <img className={style.images} src={sosGyvunaiLogo} alt="S.O.S. gyvunai" />
-            </li>
-            <li className={style.shelter}>
-              <img className={style.images} src={leseLogo} alt="Lesė" />
-            </li>
-            <li className={style.shelter}>
-              <img className={style.images} src={vggnLogo} alt="Vilniaus Gyvunu Globos Namai" />
-            </li>
-          </ul>
-        </div>
+        <MiniText text="Gyvūnų prieglaudos:" />
+        <ul className={style.shelters}>
+          <li className={style.shelter}>
+            <img className={style.images} src={sosGyvunaiLogo} alt="S.O.S. gyvunai" />
+          </li>
+          <li className={style.shelter}>
+            <img className={style.images} src={leseLogo} alt="Lesė" />
+          </li>
+          <li className={style.shelter}>
+            <img className={style.images} src={vggnLogo} alt="Vilniaus Gyvunu Globos Namai" />
+          </li>
+        </ul>
       </div>
     );
   }
